2223c995616db9b67d44fd3f64930003f900bac7
Noah <noahbresler2002@gmail.com>
2020-03-12 00:52:43 -0500

    feat(WindowFrame): add dynamic window size

--- src/main/java/com/noahbres/meepmeep/ui/WindowFrame.kt
+++ src/main/java/com/noahbres/meepmeep/ui/WindowFrame.kt
@@ -1,15 +1,28 @@
 package com.noahbres.meepmeep.ui
 
+import java.awt.FlowLayout
 import javax.swing.JFrame
 
-class WindowFrame(title: String): JFrame() {
+class WindowFrame(title: String, windowSize: Int): JFrame() {
+    var internalWidth = windowSize
+    var internalHeight = windowSize
+
+    val canvas = MainCanvas(internalWidth, internalHeight)
+
     init {
         setTitle(title)
 
         defaultCloseOperation = JFrame.EXIT_ON_CLOSE
-        setSize(600, 600)
+        setSize(internalWidth, internalHeight)
         setLocationRelativeTo(null)
 
-        add(MainCanvas())
+        isResizable = false
+
+        layout = FlowLayout(0,0,0)
+        contentPane.add(canvas)
+//        contentPane.add(JButton("test"))
+        pack()
+
+        canvas.start()
     }
 }
