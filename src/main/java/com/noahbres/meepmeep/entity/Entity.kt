7d5a63c7d01bc53ffbe7fb1d9fbea5a6b426af4e
Noah <noahbresler2002@gmail.com>
2020-03-14 07:03:49 -0500

    feat(Entity.kt): implement an entity base

---
+++ src/main/java/com/noahbres/meepmeep/entity/Entity.kt
@@ -0,0 +1,12 @@
+package com.noahbres.meepmeep.entity
+
+import java.awt.Graphics2D
+
+interface Entity {
+    val zIndex: Int
+
+    fun update(deltaTime: Long)
+    fun render(gfx: Graphics2D, canvasWidth: Int, canvasHeight: Int)
+
+    fun setCanvasDimensions(canvasWidth: Int, canvasHeight: Int)
+}
