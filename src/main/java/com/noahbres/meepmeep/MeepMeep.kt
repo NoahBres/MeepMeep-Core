79db71470f0811a5dc0e3a674ad73998564923d5
Noah <noahbresler2002@gmail.com>
2020-03-14 07:07:58 -0500

    feat(MeeMeep.kt): add default entities, draw fps, add to entity handling

--- src/main/java/com/noahbres/meepmeep/MeepMeep.kt
+++ src/main/java/com/noahbres/meepmeep/MeepMeep.kt
@@ -3,17 +3,28 @@ package com.noahbres.meepmeep
 import com.acmerobotics.roadrunner.geometry.Pose2d
 import com.acmerobotics.roadrunner.trajectory.Trajectory
 import com.noahbres.meepmeep.colorscheme.ColorManager
-import com.noahbres.meepmeep.colorscheme.Scheme
-import com.noahbres.meepmeep.gfx.DrawCurrentBot
+import com.noahbres.meepmeep.colorscheme.ColorScheme
+import com.noahbres.meepmeep.entity.AxesEntity
+import com.noahbres.meepmeep.entity.BotEntity
+import com.noahbres.meepmeep.entity.Entity
+import com.noahbres.meepmeep.entity.ThemedEntity
 import com.noahbres.meepmeep.ui.WindowFrame
-import com.noahbres.meepmeep.util.FieldUtil
 import com.noahbres.meepmeep.util.LoopManager
 import java.awt.*
+import java.awt.event.MouseListener
+import java.awt.event.MouseMotionListener
 import java.io.File
 import javax.imageio.ImageIO
 import javax.swing.UIManager
 
 class MeepMeep(val windowSize: Int) {
+    companion object {
+        @JvmStatic
+        lateinit var DEFAULT_BOT_ENTITY: BotEntity
+        @JvmStatic
+        lateinit var DEFAULT_AXES_ENTITY: AxesEntity
+    }
+
     private val windowFrame = WindowFrame("Meep Meep", windowSize)
     private val canvas = windowFrame.canvas
 
@@ -21,22 +32,14 @@ class MeepMeep(val windowSize: Int) {
 
     private val colorManager = ColorManager()
 
-    private var robotWidth = 18.0
-    private var robotHeight = 18.0
-    // TODO implement once I make my RR path splitter
-//    private var baseConstraints = DriveConstraints(
-//            30.0, 30.0, 0.0,
-//            Math.toRadians(180.0), Math.toRadians(180.0), 0.0
-//    )
+    private val trajectoryList = mutableListOf<Trajectory>();
 
-    private var trajectoryList = mutableListOf<Trajectory>();
+    private val entityList = mutableListOf<Entity>()
 
-    // Overridable drawing functions
-    private val drawDefaultBot = object : DrawCurrentBot {
-        override fun draw(gfx: Graphics2D, pose: Pose2d) {
+    // Returns true if entity list needs to be sorted
+    private var entityListDirty = false
 
-        }
-    }
+    private val fontCMUBoldLight: Font
 
     private val render: () -> Unit = {
         val g = canvas.bufferStrat.drawGraphics as Graphics2D
@@ -46,10 +49,25 @@ class MeepMeep(val windowSize: Int) {
         // render
         if (bg != null) g.drawImage(bg, 0, 0, null)
 
+        entityList.forEach { it.render(g, canvas.width, canvas.height) }
+
+        // Draw fps
+        val fpsFont = Font("Sans", Font.BOLD, 20)
+        g.font = fpsFont
+        g.color = ColorManager.COLOR_PALETTE.GREEN_600
+        g.drawString("${loopManager.fps} FPS", 10, 20)
+
         g.dispose()
         canvas.bufferStrat.show()
     }
-    private val update: (deltaTime: Long) -> Unit = {
+    private val update: (deltaTime: Long) -> Unit = { deltaTime ->
+
+        if (entityListDirty) {
+            entityList.sortBy { it.zIndex }
+            entityListDirty = false
+        }
+
+        entityList.forEach { it.update(deltaTime) }
     }
 
     private val loopManager = LoopManager(60, update, render)
@@ -57,7 +75,15 @@ class MeepMeep(val windowSize: Int) {
     init {
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName())
 
+        fontCMUBoldLight = Font.createFont(Font.TRUETYPE_FONT, File("res/font/cmunbi.ttf")).deriveFont(20f)
+
+        DEFAULT_BOT_ENTITY = BotEntity(18.0, 18.0, Pose2d(), ColorManager.DEFAULT_THEME, 0.8, windowSize, windowSize)
+        DEFAULT_AXES_ENTITY = AxesEntity(0.8, ColorManager.DEFAULT_THEME, 0.3, 0.9, windowSize, windowSize, fontCMUBoldLight, 20f)
+
         setBackground(Background.GRID_BLUE)
+
+        addEntity(DEFAULT_BOT_ENTITY)
+        addEntity(DEFAULT_AXES_ENTITY)
     }
 
     fun start(): MeepMeep {
@@ -85,11 +111,11 @@ class MeepMeep(val windowSize: Int) {
             }
             Background.FIELD_SKYSTONE_LIGHT -> {
                 colorManager.isDarkMode = false
-                ImageIO.read(File("res/background/field-skystone-light.jpg"))
+                ImageIO.read(File("res/background/field-skystone-light-fix.jpg"))
             }
             Background.FIELD_SKYSTONE_DARK -> {
                 colorManager.isDarkMode = true
-                ImageIO.read(File("res/background/field-skystone-dark.jpg"))
+                ImageIO.read(File("res/background/field-skystone-dark-fix.jpg"))
             }
         }.getScaledInstance(windowSize, windowSize, Image.SCALE_SMOOTH)
 
@@ -103,9 +129,12 @@ class MeepMeep(val windowSize: Int) {
     }
 
     @JvmOverloads
-    fun setTheme(scheme: Scheme, isDarkMode: Boolean = false): MeepMeep {
-        colorManager.theme = scheme
-        colorManager.isDarkMode = isDarkMode
+    fun setTheme(schemeLight: ColorScheme, schemeDark: ColorScheme = schemeLight): MeepMeep {
+        colorManager.setTheme(schemeLight, schemeDark)
+
+        entityList.forEach {
+            if (it is ThemedEntity) it.switchScheme(colorManager.theme)
+        }
 
         return this
     }
@@ -117,24 +146,15 @@ class MeepMeep(val windowSize: Int) {
     }
 
     //-------------Robot Settings-------------//
-//    fun setBaseConstraints(constraints: DriveConstraints): MeepMeep {
-//        baseConstraints = constraints
-//
-//        return this
-//    }
-
     fun setBotDimensions(width: Double, height: Double): MeepMeep {
-        robotWidth = width
-        robotHeight = height
+        if (DEFAULT_BOT_ENTITY in entityList) {
+            DEFAULT_BOT_ENTITY.setDimensions(width, height)
+        }
+
         return this
     }
 
     //-------------Trajectory Settings-------------//
-    // TODO implement once I make my RR path splitter
-//    fun setStartPoint(point: Pose2d): MeepMeep {
-//        return this
-//    }
-
     fun addTrajectory(trajectories: List<Trajectory>): MeepMeep {
         trajectoryList.addAll(trajectories);
 
@@ -145,12 +165,26 @@ class MeepMeep(val windowSize: Int) {
         return this
     }
 
-    // TODO implement once I make my RR path splitter
-//    fun addTrajectory(trajectoryFeedback: TrajectoryBuilderCallback): MeepMeep {
-//        return this
-//    }
-
     fun clearTrajectories(): MeepMeep {
+        return this
+    }
+
+    //-------------Entity-------------//
+    fun addEntity(entity: Entity): MeepMeep {
+        entityList.add(entity)
+        entityListDirty = true
+
+        if (entity is MouseListener) canvas.addMouseListener(entity)
+
+        if (entity is MouseMotionListener) canvas.addMouseMotionListener(entity)
+
+        return this
+    }
+
+    fun clearEntity(entity: Entity): MeepMeep {
+        entityList.remove(entity)
+        entityListDirty = true
+
         return this
     }
 
