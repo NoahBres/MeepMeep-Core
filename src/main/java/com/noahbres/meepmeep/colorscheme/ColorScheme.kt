31989829892e733a49142f81aaaed9b95c97e6ea
Noah <noahbresler2002@gmail.com>
2020-03-14 07:04:45 -0500

    feat(ColorScheme): create a color scheme base

---
+++ src/main/java/com/noahbres/meepmeep/colorscheme/ColorScheme.kt
@@ -0,0 +1,11 @@
+package com.noahbres.meepmeep.colorscheme
+
+import java.awt.Color
+
+abstract class ColorScheme {
+    abstract val BOT_BODY_COLOR: Color
+    abstract val BOT_WHEEL_COLOR: Color
+    abstract val BOT_DIRECTION_COLOR: Color
+    abstract val AXIS_X_COLOR: Color
+    abstract val AXIS_Y_COLOR: Color
+}
