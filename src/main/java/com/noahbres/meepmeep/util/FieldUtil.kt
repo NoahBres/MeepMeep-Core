8676f345f81a0025d2171584a7a860d946db95f7
Noah <noahbresler2002@gmail.com>
2020-03-15 05:03:17 -0500

    feat(FieldUtil.kt): change conversion functions to use canvas dimensions by default

--- src/main/java/com/noahbres/meepmeep/util/FieldUtil.kt
+++ src/main/java/com/noahbres/meepmeep/util/FieldUtil.kt
@@ -13,17 +13,23 @@ class FieldUtil {
         var FIELD_HEIGHT = 144
 
         @JvmStatic
-        fun screenCoordsToFieldCoords(vector2d: Vector2d, canvasWidth: Double, canvasHeight: Double): Vector2d {
+        var CANVAS_WIDTH = 0.0
+        var CANVAS_HEIGHT = 0.0
+
+        @JvmStatic
+        @JvmOverloads
+        fun screenCoordsToFieldCoords(vector2d: Vector2d, canvasWidth: Double = CANVAS_WIDTH, canvasHeight: Double = CANVAS_HEIGHT): Vector2d {
             return mirrorY(vector2d) - Vector2d(FIELD_WIDTH / 2.0, FIELD_HEIGHT / 2.0) / max(canvasWidth, canvasHeight) / FIELD_WIDTH.toDouble()
         }
 
         @JvmStatic
-        fun fieldCoordsToScreenCoords(vector2d: Vector2d, canvasWidth: Double, canvasHeight: Double): Vector2d {
+        @JvmOverloads
+        fun fieldCoordsToScreenCoords(vector2d: Vector2d, canvasWidth: Double = CANVAS_WIDTH, canvasHeight: Double = CANVAS_HEIGHT): Vector2d {
            return (mirrorY(vector2d) + Vector2d(FIELD_WIDTH / 2.0, FIELD_HEIGHT / 2.0)) * min(canvasWidth, canvasHeight) / FIELD_WIDTH.toDouble()
         }
 
         @JvmStatic
-        fun scaleInchesToPixel(inches: Double, canvasWidth: Double, canvasHeight: Double): Double {
+        fun scaleInchesToPixel(inches: Double, canvasWidth: Double = CANVAS_WIDTH, canvasHeight: Double = CANVAS_HEIGHT): Double {
             return inches / min(FIELD_WIDTH.toDouble(), FIELD_HEIGHT.toDouble()) * min(canvasWidth, canvasHeight)
         }
 
