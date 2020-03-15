c7fe238e53a939666354bbb00e43f6acbc9ec698
Noah <noahbresler2002@gmail.com>
2020-03-15 05:02:45 -0500

    feat(AnimationController.kt): finish implementation and add clipping

--- src/main/java/com/noahbres/meepmeep/anim/AnimationController.kt
+++ src/main/java/com/noahbres/meepmeep/anim/AnimationController.kt
@@ -1,33 +1,58 @@
 package com.noahbres.meepmeep.anim
 
-class AnimationController(val value: Double) {
+import kotlin.math.max
+import kotlin.math.min
+
+class AnimationController(var value: Double) {
     private var isAnimating = false
 
     private var currentStartTime = 0.0
     private var currentEndTime = 0.0
-    private var currentTarget = 0.0
+    private var currentTotalTime = 0.0
+
+    private var currentTargetStart = 0.0
     private var currentTargetDelta = 0.0
     private var currentEase = Ease.LINEAR
+
+    private var isClipped = false
+    private var lowerBound = 0.0
+    private var upperBound = 0.0
 
     fun update() {
         if(!isAnimating) return
 
-        val totalTime = currentEndTime - currentStartTime
         val elapsedTime = System.currentTimeMillis() - currentStartTime
-        val progress = currentEase(elapsedTime / totalTime)
+        val progress = currentEase(elapsedTime / currentTotalTime)
 
+        value = currentTargetStart + progress * currentTargetDelta
 
+        if(isClipped) value = max(lowerBound, min(value, upperBound))
+
+        if(progress >= 1) isAnimating = false
     }
 
-    fun anim(target: Double, time: Double, ease: (t: Double) -> Double) {
+    fun anim(target: Double, timeMs: Double, ease: (t: Double) -> Double) {
+        if(timeMs <= 0) throw Error("Animation length can not be equal or less than 0")
+
+        if(target == value) return
+
         isAnimating = true
 
+        currentTargetStart = value
         currentTargetDelta = target - value
-        currentTarget = target
 
         currentStartTime = System.currentTimeMillis().toDouble()
-        currentEndTime = currentStartTime + time
+        currentEndTime = currentStartTime + timeMs
+        currentTotalTime = currentEndTime - currentStartTime
 
         currentEase = ease
+    }
+
+    fun clip(lowerBound: Double, upperBound: Double): AnimationController {
+        this.isClipped = true
+        this.lowerBound = lowerBound
+        this.upperBound = upperBound
+
+        return this
     }
 }
