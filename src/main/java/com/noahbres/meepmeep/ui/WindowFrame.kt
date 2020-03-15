7aa803673c19ad878c15ac865b288285bbd4e39f
Noah <noahbresler2002@gmail.com>
2020-03-15 05:04:48 -0500

    feat(WindowFrame.kt): run an exit on window close to ensure close

--- src/main/java/com/noahbres/meepmeep/ui/WindowFrame.kt
+++ src/main/java/com/noahbres/meepmeep/ui/WindowFrame.kt
@@ -1,9 +1,13 @@
 package com.noahbres.meepmeep.ui
 
 import java.awt.FlowLayout
+import java.awt.event.WindowAdapter
+import java.awt.event.WindowEvent
+import java.awt.event.WindowListener
 import javax.swing.JFrame
+import kotlin.system.exitProcess
 
-class WindowFrame(title: String, windowSize: Int): JFrame() {
+class WindowFrame(title: String, windowSize: Int) : JFrame() {
     var internalWidth = windowSize
     var internalHeight = windowSize
 
@@ -12,13 +16,22 @@ class WindowFrame(title: String, windowSize: Int): JFrame() {
     init {
         setTitle(title)
 
-        defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+        defaultCloseOperation = DO_NOTHING_ON_CLOSE
+        addWindowListener(object : WindowAdapter() {
+            override fun windowClosing(we: WindowEvent?) {
+                super.windowClosing(we)
+
+                dispose()
+                exitProcess(0)
+            }
+        })
+
         setSize(internalWidth, internalHeight)
         setLocationRelativeTo(null)
 
         isResizable = false
 
-        layout = FlowLayout(0,0,0)
+        layout = FlowLayout(0, 0, 0)
         contentPane.add(canvas)
 //        contentPane.add(JButton("test"))
         pack()
