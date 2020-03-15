c94d378d9c128b026e00d4c0b1a4351c6e0846d8
Noah <noahbresler2002@gmail.com>
2020-03-15 05:05:51 -0500

    feat(ColorScheme.kt): add axis normal/hover opacity

--- src/main/java/com/noahbres/meepmeep/colorscheme/ColorScheme.kt
+++ src/main/java/com/noahbres/meepmeep/colorscheme/ColorScheme.kt
@@ -6,6 +6,9 @@ abstract class ColorScheme {
     abstract val BOT_BODY_COLOR: Color
     abstract val BOT_WHEEL_COLOR: Color
     abstract val BOT_DIRECTION_COLOR: Color
+
     abstract val AXIS_X_COLOR: Color
     abstract val AXIS_Y_COLOR: Color
+    abstract val AXIS_NORMAL_OPACITY: Double
+    abstract val AXIS_HOVER_OPACITY: Double
 }
